56c1d8df11448bb80d923705ec400a3b57e068d3
Atanu-Sarkar <atanu.noida@gmail.com>
2016-04-09 18:40:38 +0530

    Solving Expressions
    Java Session 1 Assignment4

---
+++ Expressions.java
@@ -0,0 +1,28 @@
+package com.acadgild.session.one;
+
+public class Expressions {
+
+	public static void main(String[] args) {
+		
+		int x =5;
+		int y=10;
+		//a. x + y * 2
+		int z1;
+		z1 = x + y * 2;
+	    System.out.println(z1);
+		//b. x - y + 2 
+	    int z2;
+	    z2 = x - y +2;
+	    System.out.println(z2);
+	    //c. (x+y) * 2
+	    int z3;
+	    z3 = (x + y) * 2;
+	    System.out.println(z3);
+		//d. y % x
+	    int z4;
+	    z4 = y % x;
+	    System.out.println(z4);
+	    
+	}
+
+}
